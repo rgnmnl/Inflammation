ef6d2540a88307ac1d21c80d65d2dcea17e4ea34
Regina Manansala <manansalar@gmail.com>
2021-03-08 10:05:10 -0600

    Add fine mapping analysis code

---
+++ Fine_Mapping_Analysis.R
@@ -0,0 +1,43 @@
+##############################################################################
+## Title: Fine Mapping Analysis for IL6
+## Reference: https://www.biorxiv.org/content/10.1101/2020.01.17.910497v1
+## Author: Regina Manansala
+## Date Created: 07-October-2019
+## Date Modified: 09-October-2019
+##############################################################################
+
+library(data.table)
+library(dplyr)
+
+## Get result for rs6734238
+min <- 113083453 - 250000
+max <- 113083453 + 250000
+encore_result <- fread("il6_encore_results.txt") %>% subset(., CHR == 'chr2' & POS %between% c(min,max))
+
+# head(encore_result) 
+
+## Calculate variance
+encore_result$V <- (encore_result$SE)^2
+
+## Calculate Z score
+encore_result$Z <- encore_result$BETA/encore_result$SE
+
+## Specify prior
+W <- .04
+
+## Calculate approximate Bayes Factor
+encore_result$aBF <- sqrt(encore_result$V/encore_result$V+W) * exp((encore_result$Z^2 * W)/(2*(encore_result$V + W)))
+
+## Calculate PPi. Sort and subset data
+encore_result$PPi <- encore_result$aBF/sum(encore_result$aBF)
+encore_result <- encore_result[order(-encore_result$PPi),]
+
+N <- 0
+keep_rows <- data.frame()
+for(i in 1:nrow(encore_result)){
+  PPi <- encore_result[i]$PPi
+  N <- N + PPi
+  keep_rows <- rbind(keep_rows, encore_result[i])
+  if(N > .95) break
+}
+
